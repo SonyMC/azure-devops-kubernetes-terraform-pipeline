5123a17c26e6916d4d0c88481db9d410bf18d854
SonyMC <mailsonymathew@gmail.com>
2022-06-20 11:26:05 +0530

    Jun20_2022_v4

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -42,11 +42,19 @@ data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.mailsonymathew-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.mailsonymathew-cluster.cluster_id
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+#  load_config_file       = false
 #  version                = "~> 1.9"
 }
 
@@ -80,13 +88,7 @@ module "mailsonymathew-cluster" {
 
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.mailsonymathew-cluster.cluster_id
-}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.mailsonymathew-cluster.cluster_id
-}
 
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
